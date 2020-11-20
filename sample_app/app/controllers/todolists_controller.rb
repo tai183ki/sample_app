eb1ae174a71b9e05e684788aacad5322ffc27afd
EC2 Default User <ec2-user@ip-172-31-50-116.ec2.internal>
2020-11-20 03:39:03 +0000

    [Finish]2-6

--- sample_app/app/controllers/todolists_controller.rb
+++ sample_app/app/controllers/todolists_controller.rb
@@ -4,7 +4,6 @@ class TodolistsController < ApplicationController
      # Viewへ渡すためのインスタンス変数に空のモデルオブジェクトを生成する。
      @list = List.new
   end
-
   def create
     # １. データを新規登録するためのインスタンス作成
     list = List.new(list_params)
@@ -13,13 +12,19 @@ class TodolistsController < ApplicationController
      # 詳細画面へリダイレクト
     redirect_to todolist_path(list.id)
   end
-
   def index
     @lists = List.all
   end
-
   def show
     @list = List.find(params[:id])
+  end
+  def edit
+    @list = List.find(params[:id])
+  end
+  def update
+    list = List.find(params[:id])
+    list.update(list_params)
+    redirect_to todolist_path(list.id)
   end
 
   private
@@ -27,5 +32,4 @@ class TodolistsController < ApplicationController
   def list_params
     params.require(:list).permit(:title, :body)
   end
-
 end
